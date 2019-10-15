d7b00dd3724cab783037a1b32df2fee33f0bfbd2
Pierre GUTHAUSER <pguthaus@e3r2p13.42.fr>
2019-10-15 18:20:44 +0200

    Prepared fmt parser

--- lib/fmt/fmt.c
+++ lib/fmt/fmt.c
@@ -6,25 +6,38 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 15:54:03 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/10/15 18:09:23 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/10/15 18:20:35 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fmt.h"
+
+static t_fmt		get_initial_fmt()
+{
+	return ((t_fmt){ 0, 0, 0 });
+}
+
+static t_fmt		parse_flags(t_state *state, t_fmt fmt)
+{
+}
+
+static t_fmt		parse_minwidth(t_state *state, t_fmt fmt)
+{
+}
+
+static t_fmt		parse_precision(t_state *state, t_fmt fmt)
+{
+}
 
 void				fmt(t_state *state)
 {
 	t_fmt			fmt;
 
 	state->frmt++;
-	fmt.flags = 0;
-	fmt.precision = 0;
-	fmt.convert_func = 0;
-	while (g_flags[(unsigned char)*state->frmt])
-	{
-		fmt.flags |= g_flags[(unsigned char)*state->frmt];
-		state->frmt++;
-	}
+	fmt = get_initial_fmt();
+	fmt = parse_flags(state, fmt);
+	fmt = parse_minwidth(state, fmt);
+	fmt = parse_precision(state, fmt);
 	if (!(fmt.convert_func = g_conversions[(unsigned char)*state->frmt]))
 		return ;
 	state->frmt++;
