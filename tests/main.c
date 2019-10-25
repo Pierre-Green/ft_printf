50632d742b2cc1c6e8369c399190c47916ab46a0
PierreGreen <pierreguthauser@gmail.com>
2019-10-25 19:18:55 +0200

    Updated libft

--- tests/main.c
+++ tests/main.c
@@ -6,20 +6,20 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 16:05:13 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/10/25 18:22:51 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/10/25 19:00:03 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.h"
 
-int		main(void)
+int		main(int argc, char **argv)
 {
 	// test_static();
 	// test_fmt_char();
 	// test_fmt_str();
 	// test_fmt_ptr();
 	// test_fmt_int();
-	test_fmt_hex();
+	// test_fmt_hex();
 	// test_fmt_uint();
 	// test_fmt_poly();
 }
