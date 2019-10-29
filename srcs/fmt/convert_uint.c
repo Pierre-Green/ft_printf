ce3852200251606f4a29c6e474b6eee81b756f83
PierreGreen <pierreguthauser@gmail.com>
2019-10-29 13:05:37 +0100

    Cleaned uint conversion

--- srcs/fmt/convert_uint.c
+++ srcs/fmt/convert_uint.c
@@ -6,7 +6,7 @@
 /*   By: pguthaus <pguthaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 20:10:43 by pguthaus          #+#    #+#             */
-/*   Updated: 2019/10/29 12:15:17 by pguthaus         ###   ########.fr       */
+/*   Updated: 2019/10/29 13:04:34 by pguthaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ static void					convert_uint_negativ(t_state *state, t_fmt fmt, size_t len)
 {
 	size_t					minwidth;
 
-	if (fmt.precision < 0)
-		fmt.minwidth = 0;
 	minwidth = MAX(len, fmt.minwidth);
 	state->count += write_uint(fmt.value.u, state->buff, len);
 	state->count += buff_write_nchar(state->buff, minwidth - len, ' ');
@@ -51,11 +49,8 @@ static void					convert_uint_default(t_state *state, t_fmt fmt, size_t len)
 	size_t					minwidth;
 
 	minwidth = MAX(len, fmt.minwidth);
-	if (fmt.precision < 0 && !(fmt.minwidth = 0))
-		convert_uint_negativ(state, fmt, len);
-	else
-		state->count += buff_write_nchar(state->buff, minwidth - len, ' ')
-			+ write_uint(fmt.value.u, state->buff, len);
+	state->count += buff_write_nchar(state->buff, minwidth - len, ' ')
+		+ write_uint(fmt.value.u, state->buff, len);
 }
 
 void						convert_uint(t_state *state, t_fmt fmt)
@@ -66,9 +61,11 @@ void						convert_uint(t_state *state, t_fmt fmt)
 	len = ft_count_uint_base(value, 10);
 	if (fmt.precision >= 0)
 		len = MAX(len, fmt.precision);
+	else if (fmt.precision < 0)
+		fmt.minwidth = ABS(fmt.precision);
 	if (value == 0 && fmt.precised && fmt.precision == 0)
 		len = 0;
-	if (fmt.flags & FLAG_NEGATIV)
+	if (fmt.flags & FLAG_NEGATIV || fmt.precision < 0)
 		convert_uint_negativ(state, fmt, len);
 	else if (fmt.flags & FLAG_ZEROPAD && !fmt.precised)
 		convert_uint_zeropad(state, fmt, len);
